24ba3c38378acd40e97e2fe5d7cc0a3760ed13ab
최지환 <cjh87467@gmail.com>
2023-01-28 15:17:19 +0900

    test

--- src/test/java/mokindang/jubging/project_backend/ProjectBackendApplicationTests.java
+++ src/test/java/mokindang/jubging/project_backend/ProjectBackendApplicationTests.java
@@ -3,7 +3,7 @@ package mokindang.jubging.project_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProjectBackendApplication.class)
 class ProjectBackendApplicationTests {
 
     @Test
