e87c57420440fa0e2cf796f15a941bc1abe8eb6c
mino <koho104777@gmail.com>
2023-01-28 16:05:08 +0900

    test

--- src/test/java/mokindang/jubging/project_backend/ProjectBackendApplicationTests.java
+++ src/test/java/mokindang/jubging/project_backend/ProjectBackendApplicationTests.java
@@ -2,8 +2,10 @@ package mokindang.jubging.project_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:/application-test.yml")
 class ProjectBackendApplicationTests {
 
     @Test
