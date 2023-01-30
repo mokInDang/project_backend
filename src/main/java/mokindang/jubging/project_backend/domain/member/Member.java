ae445afc16681edb85fb26db3d6bfe1218deaf9c
최지환 <cjh87467@gmail.com>
2023-01-30 17:25:25 +0900

    refactor : #22 코드 원복

--- src/main/java/mokindang/jubging/project_backend/domain/member/Member.java
+++ src/main/java/mokindang/jubging/project_backend/domain/member/Member.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -16,4 +17,25 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String email;
+
+    private String alias;
+
+    public Member(final String email, final String alias) {
+        this.email = email;
+        this.alias = alias;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
