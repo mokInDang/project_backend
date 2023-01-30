277630d3a2d3ab9d3ac60ba31bdd01a475dabc9a
최지환 <cjh87467@gmail.com>
2023-01-30 22:21:49 +0900

    refactor : #22 코드 원복

--- src/test/java/mokindang/jubging/project_backend/ProjectBackendApplicationTests.java
+++ src/test/java/mokindang/jubging/project_backend/ProjectBackendApplicationTests.java
@@ -1,9 +1,9 @@
 package mokindang.jubging.project_backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+@SpringBootApplication
 class ProjectBackendApplicationTests {
 
     @Test
