ac280cbbaf56f109cf5cd93eba94a200a60391e1
최지환 <cjh87467@gmail.com>
2023-01-30 17:22:23 +0900

    refactor : #22 생성자 접근 제어 레벨 변경

--- src/test/java/mokindang/jubging/project_backend/domain/member/MemberTest.java
+++ src/test/java/mokindang/jubging/project_backend/domain/member/MemberTest.java
@@ -1,25 +1,4 @@
 package mokindang.jubging.project_backend.domain.member;
 
-import org.assertj.core.api.SoftAssertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 class MemberTest {
-
-    @Test
-    @DisplayName("멤버의 email 과 alias 를 반환한다.")
-    void getter() {
-        //given
-        SoftAssertions softly = new SoftAssertions();
-        Member member = new Member("cjh87467@gmail.com", "지환");
-
-        //when
-        String email = member.getEmail();
-        String alias = member.getAlias();
-
-        //then
-        softly.assertThat(email).isEqualTo("cjh87467@gmail.com");
-        softly.assertThat(alias).isEqualTo("지환");
-        softly.assertAll();
-    }
 }
