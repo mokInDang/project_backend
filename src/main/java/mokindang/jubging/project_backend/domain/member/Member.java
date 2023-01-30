97886e91bb8d279891c237fab9dbe2cc56e97ba4
최지환 <cjh87467@gmail.com>
2023-01-30 17:19:27 +0900

    refactor : #22 생성자 접근 제어 레벨 변경

--- src/main/java/mokindang/jubging/project_backend/domain/member/Member.java
+++ src/main/java/mokindang/jubging/project_backend/domain/member/Member.java
@@ -5,11 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
 
     @Id
@@ -17,25 +16,4 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String email;
-
-    private String alias;
-
-    public Member(final String email, final String alias) {
-        this.email = email;
-        this.alias = alias;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final Member member = (Member) o;
-        return Objects.equals(id, member.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
