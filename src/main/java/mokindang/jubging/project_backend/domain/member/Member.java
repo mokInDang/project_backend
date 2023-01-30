4cc331c74352b078b14f8d01612949b8951b7c8f
최지환 <cjh87467@gmail.com>
2023-01-30 15:38:23 +0900

    feat : #22 Member 구현

---
+++ src/main/java/mokindang/jubging/project_backend/domain/member/Member.java
@@ -0,0 +1,41 @@
+package mokindang.jubging.project_backend.domain.member;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String alias;
+
+    public Member(final String email, final String alias) {
+        this.email = email;
+        this.alias = alias;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
