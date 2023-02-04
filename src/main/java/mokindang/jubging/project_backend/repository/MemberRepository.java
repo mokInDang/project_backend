a5b80af9a50f16d0652ac7a48a6bf2da9f778f13
mino <koho104777@gmail.com>
2023-02-04 18:52:56 +0900

    feat: #33 Member Repository 구현

---
+++ src/main/java/mokindang/jubging/project_backend/repository/MemberRepository.java
@@ -0,0 +1,41 @@
+package mokindang.jubging.project_backend.repository;
+
+import lombok.RequiredArgsConstructor;
+import mokindang.jubging.project_backend.domain.member.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class,id);
+    }
+
+    public Member findOneByEmail(String email) {
+        return em.createQuery("select m from Member m where m.email = :email", Member.class)
+                .setParameter("email", email)
+                .getSingleResult();
+    }
+
+    public List<Member> findByAlias(String alias) {
+        return em.createQuery("select m from Member m where m.alias = :alias", Member.class)
+                .setParameter("alias", alias)
+                .getResultList();
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+}
