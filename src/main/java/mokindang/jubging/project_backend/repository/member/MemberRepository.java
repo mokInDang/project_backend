f8aae2694c7649ace4e67467221ffe97ae53ad38
mino <koho104777@gmail.com>
2023-02-12 02:27:03 +0900

    feat: #33 Member Repository 구현

--- src/main/java/mokindang/jubging/project_backend/repository/member/MemberRepository.java
+++ src/main/java/mokindang/jubging/project_backend/repository/member/MemberRepository.java
@@ -9,5 +9,6 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findOneByEmail(String email);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByEmail(String email);
 }
