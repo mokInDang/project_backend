0d988b0b34cb6c4e3a10c7345d509b3ff9ca871b
mino <koho104777@gmail.com>
2023-02-12 02:19:57 +0900

    feat: #33 Member Repository 구현

--- src/main/java/mokindang/jubging/project_backend/repository/member/MemberRepository.java
+++ src/main/java/mokindang/jubging/project_backend/repository/member/MemberRepository.java
@@ -1,12 +1,9 @@
-package mokindang.jubging.project_backend.repository;
+package mokindang.jubging.project_backend.repository.member;
 
-import lombok.RequiredArgsConstructor;
 import mokindang.jubging.project_backend.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
