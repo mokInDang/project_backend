4f6c35b42e030f317595b5a4a17a4118102c3fe0
mino <koho104777@gmail.com>
2023-02-12 01:02:31 +0900

    feat:#33 Member Repository Spring Data JPA 적용 및 리뷰 적용

--- src/main/java/mokindang/jubging/project_backend/repository/MemberRepository.java
+++ src/main/java/mokindang/jubging/project_backend/repository/MemberRepository.java
@@ -1,16 +1,13 @@
 package mokindang.jubging.project_backend.repository;
 
-import lombok.RequiredArgsConstructor;
 import mokindang.jubging.project_backend.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findOneByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
