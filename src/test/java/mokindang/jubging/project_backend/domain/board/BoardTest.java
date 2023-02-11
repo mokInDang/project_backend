0dd61aade243e898f48619e1d6315b009ed67576
최지환 <cjh87467@gmail.com>
2023-02-11 22:47:40 +0900

    test : #31 Board 테스트 코드 작성

--- src/test/java/mokindang/jubging/project_backend/domain/board/BoardTest.java
+++ src/test/java/mokindang/jubging/project_backend/domain/board/BoardTest.java
@@ -1,33 +1,36 @@
-//package mokindang.jubging.project_backend.domain.board;
-//
-//import mokindang.jubging.project_backend.domain.member.Member;
-//import org.assertj.core.api.SoftAssertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.test.annotation.Rollback;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class BoardTest {
-//
-//    @Test
-//    @DisplayName("게시판의 member(작성자), title, content 를 반환한다.")
-//    public void getter() {
-//        //given
-//        SoftAssertions softly = new SoftAssertions();
-//        Member testMember = new Member("koho1047@naver.com", "민호");
-//        Board board = new Board(testMember,"게시판 제목", "게시판 내용 작성 테스트");
-//
-//        //when
-//        Member member = board.getMember();
-//        String title = board.getTitle();
-//        String content = board.getContent();
-//
-//        //then
-//        softly.assertThat(member).isEqualTo(testMember);
-//        softly.assertThat(title).isEqualTo("게시판 제목");
-//        softly.assertThat(content).isEqualTo("게시판 내용 작성 테스트");
-//        softly.assertAll();
-//    }
-//
-//}
+package mokindang.jubging.project_backend.domain.board;
+
+import mokindang.jubging.project_backend.domain.member.Member;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class BoardTest {
+
+    @Test
+    @DisplayName("게시판의 member(작성자), title, content 를 반환한다.")
+    void getter() {
+        //given
+        SoftAssertions softly = new SoftAssertions();
+        Member testMember = new Member("koho1047@naver.com", "민호");
+        Board board = new Board(testMember, LocalDate.of(2023, 2, 11),
+                "달리기", "게시판 제목", "게시판 내용 작성 테스트");
+
+        //when
+        Member member = board.getMember();
+        Title title = board.getTitle();
+        Content content = board.getContent();
+        ActivityCategory activityCategory = board.getActivityCategory();
+        LocalDate startDate = board.getStartDate();
+
+        //then
+        softly.assertThat(member).isEqualTo(testMember);
+        softly.assertThat(activityCategory).isSameAs(ActivityCategory.RUNNING);
+        softly.assertThat(startDate).isEqualTo(LocalDate.of(2023, 2, 11));
+        softly.assertThat(title).isEqualTo(new Title("게시판 제목"));
+        softly.assertThat(content).isEqualTo(new Content("게시판 내용 작성 테스트"));
+        softly.assertAll();
+    }
+}
