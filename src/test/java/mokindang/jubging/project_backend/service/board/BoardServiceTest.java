7e97b0d633e17b19901db3836a0ab800169672e6
최지환 <cjh87467@gmail.com>
2023-02-12 11:31:42 +0900

    test : #41 BoardService 테스트코드 작성

---
+++ src/test/java/mokindang/jubging/project_backend/service/board/BoardServiceTest.java
@@ -0,0 +1,63 @@
+package mokindang.jubging.project_backend.service.board;
+
+import mokindang.jubging.project_backend.domain.member.Member;
+import mokindang.jubging.project_backend.repository.board.BoardRepository;
+import mokindang.jubging.project_backend.service.board.request.BoardCreateRequest;
+import mokindang.jubging.project_backend.service.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class BoardServiceTest {
+
+    @Mock
+    private MemberService memberService;
+
+    @Mock
+    private BoardRepository boardRepository;
+
+    @InjectMocks
+    private BoardService boardService;
+
+
+    @Test
+    @DisplayName("해당하는 유저가 작성한 게시글을 저장한다.")
+    void write() {
+        //given
+        Member member = mock(Member.class);
+        when(memberService.findByMemberId(anyLong())).thenReturn(member);
+
+        BoardCreateRequest boardCreateRequest = new BoardCreateRequest("제목", "본문내용", "달리기",
+                LocalDate.of(2023, 2, 12));
+
+        //when
+        boardService.write(1L, boardCreateRequest);
+
+        //then
+        verify(boardRepository, times(1)).save(any());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 유저가 게시글 저장 시 , 예외를 발생한다.")
+    void writeFailedByNonexistentMember() {
+        //given
+        when(memberService.findByMemberId(anyLong())).thenThrow(new IllegalArgumentException("해당하는 유저가 존재하지 않습니다."));
+
+        BoardCreateRequest boardCreateRequest = new BoardCreateRequest("제목", "본문내용", "달리기",
+                LocalDate.of(2023, 2, 12));
+
+        //when, then
+        assertThatThrownBy(() -> boardService.write(1L, boardCreateRequest))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당하는 유저가 존재하지 않습니다.");
+    }
+}
