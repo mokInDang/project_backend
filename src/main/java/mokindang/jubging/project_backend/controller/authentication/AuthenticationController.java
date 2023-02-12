0d2ba127877a7880c863e02a1236dc4c0bcd86b4
mino <koho104777@gmail.com>
2023-02-12 23:45:53 +0900

    feat: #37 상태코드 200 201 분기 구현

--- src/main/java/mokindang/jubging/project_backend/controller/authentication/AuthenticationController.java
+++ src/main/java/mokindang/jubging/project_backend/controller/authentication/AuthenticationController.java
@@ -2,8 +2,12 @@ package mokindang.jubging.project_backend.controller.authentication;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import mokindang.jubging.project_backend.domain.member.LoginResponseDto;
+import mokindang.jubging.project_backend.service.member.dto.LoginResponseDto;
+import mokindang.jubging.project_backend.domain.member.MemberState;
+import mokindang.jubging.project_backend.service.member.dto.MemberStateDto;
 import mokindang.jubging.project_backend.service.member.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -14,7 +18,14 @@ public class AuthenticationController {
     private final MemberService memberService;
 
     @GetMapping("api/member/join")
-    public LoginResponseDto kakaoCallback(@RequestParam String code) {
-        return memberService.login(code);
+    public ResponseEntity<LoginResponseDto> kakaoCallback(@RequestParam String code) {
+        MemberStateDto memberStateDto = memberService.login(code);
+        LoginResponseDto loginResponseDto = new LoginResponseDto(memberStateDto.getAccessToken(),
+                                                memberStateDto.getRefreshToken(), memberStateDto.getAlias());
+
+        if (memberStateDto.getMemberState() == MemberState.JOIN) {
+            return new ResponseEntity<>(loginResponseDto, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(loginResponseDto,HttpStatus.OK);
     }
 }
