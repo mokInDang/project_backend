9eb6a7854a1056d9814ef2a83b2c8ee2f08b69e8
최지환 <cjh87467@gmail.com>
2023-02-12 11:32:27 +0900

    feat : #41 BoardService 구현

---
+++ src/main/java/mokindang/jubging/project_backend/service/board/BoardService.java
@@ -0,0 +1,27 @@
+package mokindang.jubging.project_backend.service.board;
+
+import lombok.RequiredArgsConstructor;
+import mokindang.jubging.project_backend.domain.board.Board;
+import mokindang.jubging.project_backend.domain.member.Member;
+import mokindang.jubging.project_backend.repository.board.BoardRepository;
+import mokindang.jubging.project_backend.service.board.request.BoardCreateRequest;
+import mokindang.jubging.project_backend.service.member.MemberService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final MemberService memberService;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public void write(final Long memberId, final BoardCreateRequest boardCreateRequest) {
+        Member member = memberService.findByMemberId(memberId);
+        Board board = new Board(member, boardCreateRequest.getStartDate(), boardCreateRequest.getActivityCategory(),
+                boardCreateRequest.getTitle(), boardCreateRequest.getContent());
+        boardRepository.save(board);
+    }
+}
