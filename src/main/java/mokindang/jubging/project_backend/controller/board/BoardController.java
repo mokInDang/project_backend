b95f187469a12376e96d88789fe5f074272f4679
최지환 <cjh87467@gmail.com>
2023-02-14 15:40:14 +0900

    feat : #41 BoardController 구현

--- src/main/java/mokindang/jubging/project_backend/controller/board/BoardController.java
+++ src/main/java/mokindang/jubging/project_backend/controller/board/BoardController.java
@@ -1,19 +1,29 @@
 package mokindang.jubging.project_backend.controller.board;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mokindang.jubging.project_backend.service.board.BoardService;
 import mokindang.jubging.project_backend.service.board.request.BoardCreateRequest;
 import mokindang.jubging.project_backend.web.argumentresolver.Login;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("/api/boards")
+@RequestMapping("/api/boards")
+@RestController
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
 
     @PostMapping
     public ResponseEntity<Void> write(@Login Long memberId, final BoardCreateRequest boardCreateRequest) {
         log.info("memberId = {} 의 새글작성", memberId);
-        return null;
+        boardService.write(memberId, boardCreateRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .build();
     }
 }
