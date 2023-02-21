1b05bd1c4e717c8589132a3c8c3b256180ee5c2d
mino <koho104777@gmail.com>
2023-02-21 12:02:11 +0900

    feat: #43 RefreshToken Repository 구현

---
+++ src/main/java/mokindang/jubging/project_backend/repository/token/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package mokindang.jubging.project_backend.repository.token;
+
+import mokindang.jubging.project_backend.domain.token.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByMemberId(Long memberId);
+
+    Optional<RefreshToken> findByToken(String token);
+}
