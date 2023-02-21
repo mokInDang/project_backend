fc7d9901b2f6d1b6b4171c2cf94a6d20f309562c
최지환 <cjh87467@gmail.com>
2023-02-21 20:19:18 +0900

    refactor : #50 스웨거 어노테이션 설정 값 변경 및 스키마 어노테이션 추가

--- src/main/java/mokindang/jubging/project_backend/controller/board/BoardController.java
+++ src/main/java/mokindang/jubging/project_backend/controller/board/BoardController.java
@@ -1,11 +1,14 @@
 package mokindang.jubging.project_backend.controller.board;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mokindang.jubging.project_backend.exception.ErrorResponse;
 import mokindang.jubging.project_backend.service.board.BoardService;
 import mokindang.jubging.project_backend.service.board.request.BoardCreateRequest;
 import mokindang.jubging.project_backend.web.argumentresolver.Login;
@@ -28,10 +31,10 @@ public class BoardController {
     @Operation(summary = "새글작성")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "새글작성"),
-            @ApiResponse(responseCode = "400", description = "유효하지 않은 유저"),
-            @ApiResponse(responseCode = "400", description = "유효하지 않은 본문내용"),
-            @ApiResponse(responseCode = "400", description = "유효하지 않은 제목"),
-            @ApiResponse(responseCode = "400", description = "유효하지 않은 활동 시작일")
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 유저 \t\n" +
+                    "유효하지 않은 본문내용 \t\n" +
+                    "유효하지 않은 제목 \t\n" +
+                    "유효하지 않은 활동 시작일", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
     @PostMapping
     public ResponseEntity<Void> write(@Login Long memberId, @RequestBody final BoardCreateRequest boardCreateRequest) {
--- src/main/java/mokindang/jubging/project_backend/exception/ErrorResponse.java
+++ src/main/java/mokindang/jubging/project_backend/exception/ErrorResponse.java
@@ -1,5 +1,6 @@
 package mokindang.jubging.project_backend.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,5 +8,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ErrorResponse {
 
+    @Schema(name = "에러 메세지")
     private final String error;
 }
