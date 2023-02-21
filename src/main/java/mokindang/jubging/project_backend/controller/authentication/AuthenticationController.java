80914b21ad8499cb70d7d696352ce253b00f988d
mino <koho104777@gmail.com>
2023-02-21 11:41:50 +0900

    feat: #43 토큰 재요청 추가

--- src/main/java/mokindang/jubging/project_backend/controller/authentication/AuthenticationController.java
+++ src/main/java/mokindang/jubging/project_backend/controller/authentication/AuthenticationController.java
@@ -2,15 +2,15 @@ package mokindang.jubging.project_backend.controller.authentication;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import mokindang.jubging.project_backend.domain.member.MemberState;
+import mokindang.jubging.project_backend.domain.member.LoginState;
+import mokindang.jubging.project_backend.service.dto.JwtResponse;
+import mokindang.jubging.project_backend.service.dto.RefreshTokenRequest;
 import mokindang.jubging.project_backend.service.member.MemberService;
-import mokindang.jubging.project_backend.service.member.dto.LoginResponseDto;
-import mokindang.jubging.project_backend.service.member.dto.LoginStateDto;
+import mokindang.jubging.project_backend.service.dto.LoginResponseDto;
+import mokindang.jubging.project_backend.service.dto.LoginStateDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -25,11 +25,21 @@ public class AuthenticationController {
         LoginResponseDto loginResponseDto =
                 new LoginResponseDto(loginStateDto.getAccessToken(), loginStateDto.getRefreshToken(), loginStateDto.getAlias());
 
-        if (loginStateDto.getMemberState() == MemberState.JOIN) {
+        if (loginStateDto.getLoginState() == LoginState.JOIN) {
             return ResponseEntity.status(HttpStatus.CREATED)
+                    .header("Authorization", loginStateDto.getAccessToken(), loginStateDto.getRefreshToken())
                     .body(loginResponseDto);
         }
         return ResponseEntity.ok()
+                .header("Authorization", loginStateDto.getAccessToken(), loginStateDto.getRefreshToken())
                 .body(loginResponseDto);
+    }
+
+    @PostMapping("/api/member/reissueToken")
+    public ResponseEntity<JwtResponse> reissueJwtToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+        JwtResponse jwtResponse = memberService.reissue(refreshTokenRequest);
+        return ResponseEntity.ok()
+                .header("Authorization", jwtResponse.getAccessToken(), jwtResponse.getRefreshToken())
+                .build();
     }
 }
