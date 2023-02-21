cf06efb34712e66339c9a009a6dd2e581230b133
mino <koho104777@gmail.com>
2023-02-21 11:44:21 +0900

    feat: #43 로그인 체크 인터셉터 추가

--- src/main/java/mokindang/jubging/project_backend/web/config/AuthorizationConfiguration.java
+++ src/main/java/mokindang/jubging/project_backend/web/config/AuthorizationConfiguration.java
@@ -2,8 +2,10 @@ package mokindang.jubging.project_backend.web.config;
 
 import lombok.RequiredArgsConstructor;
 import mokindang.jubging.project_backend.web.argumentresolver.LoginMemberArgumentResolver;
+import mokindang.jubging.project_backend.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,9 +16,18 @@ public class AuthorizationConfiguration implements WebMvcConfigurer {
 
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final LoginCheckInterceptor loginCheckInterceptor;
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckInterceptor)
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/index.html", "/api/member/join/**");
     }
 }
