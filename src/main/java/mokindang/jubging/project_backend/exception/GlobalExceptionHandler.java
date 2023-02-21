02f756ba4773586b3ddbf2c9649829d225417ea0
mino <koho104777@gmail.com>
2023-02-21 11:45:50 +0900

    feat: #43 JwtException 예외 처리 추가

--- src/main/java/mokindang/jubging/project_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/mokindang/jubging/project_backend/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package mokindang.jubging.project_backend.exception;
 
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +16,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(final RuntimeException e) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ErrorResponse> handleJwtException(final RuntimeException e) {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse(e.getMessage()));
     }
