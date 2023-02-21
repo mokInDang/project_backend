05e4d33d9ac9f11c0d2123a06ff7db0b4e6bc227
mino <koho104777@gmail.com>
2023-02-21 11:56:36 +0900

    refactor: #43 @JsonIgnore 추가

--- src/main/java/mokindang/jubging/project_backend/service/dto/LoginResponseDto.java
+++ src/main/java/mokindang/jubging/project_backend/service/dto/LoginResponseDto.java
@@ -1,5 +1,6 @@
-package mokindang.jubging.project_backend.service.member.dto;
+package mokindang.jubging.project_backend.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +8,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LoginResponseDto {
 
+    @JsonIgnore
     private final String accessToken;
+
+    @JsonIgnore
     private final String refreshToken;
+
     private final String alias;
 }
