12d21fba3448f1fc7c304fd5bf9dd87d51f59491
mino <koho104777@gmail.com>
2023-02-22 20:40:43 +0900

    refactor : #67 @JsonIgnore 추가

--- src/main/java/mokindang/jubging/project_backend/service/member/response/KakaoApiMemberResponse.java
+++ src/main/java/mokindang/jubging/project_backend/service/member/response/KakaoApiMemberResponse.java
@@ -1,14 +1,23 @@
 package mokindang.jubging.project_backend.service.member.response;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class KakaoApiMemberResponse {
-    private final String accessToken;
-    private final String refreshToken;
+
+    @JsonIgnore
+    private final String kakaoAccessToken;
+
+    @JsonIgnore
+    private final String kakaoRefreshToken;
+
+    @JsonIgnore
     private final String alias;
+
+    @JsonIgnore
     private final String email;
 }
--- src/main/java/mokindang/jubging/project_backend/service/member/response/LoginStateResponse.java
+++ src/main/java/mokindang/jubging/project_backend/service/member/response/LoginStateResponse.java
@@ -1,5 +1,6 @@
 package mokindang.jubging.project_backend.service.member.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import mokindang.jubging.project_backend.domain.member.LoginState;
@@ -8,8 +9,15 @@ import mokindang.jubging.project_backend.domain.member.LoginState;
 @RequiredArgsConstructor
 public class LoginStateResponse {
 
+    @JsonIgnore
     private final String accessToken;
+
+    @JsonIgnore
     private final String refreshToken;
+
+    @JsonIgnore
     private final String alias;
+
+    @JsonIgnore
     private final LoginState loginState;
 }
