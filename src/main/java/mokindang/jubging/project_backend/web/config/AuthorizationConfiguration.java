3b0767176f2c5710c6bebc59f04b921136b6d1ba
최지환 <cjh87467@gmail.com>
2023-02-26 17:55:06 +0900

    style : #67 개행 추가

--- src/main/java/mokindang/jubging/project_backend/web/config/AuthorizationConfiguration.java
+++ src/main/java/mokindang/jubging/project_backend/web/config/AuthorizationConfiguration.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Configuration
 @RequiredArgsConstructor
 public class AuthorizationConfiguration implements WebMvcConfigurer {
+
     private static final List<String> MEMBER_SERVICES_PATTERNS = List.of("/", "/api/member/join/**");
     private static final List<String> KAKAO_LOGIN_PATTERNS = List.of("/index.html", "/kakao_login_medium_narrow.png");
     private static final List<String> SWAGGER_PATTERNS = List.of("/v3/api-docs", "/swagger-ui.html", "/swagger-ui/index.html",
