1a9964c9226bbb187dbe75b16e3032b20e163dd3
mino <koho104777@gmail.com>
2023-03-03 14:10:18 +0900

    refactor : #54 Lombok 추가

--- src/main/java/mokindang/jubging/project_backend/service/member/request/RegionRequest.java
+++ src/main/java/mokindang/jubging/project_backend/service/member/request/RegionRequest.java
@@ -1,11 +1,13 @@
 package mokindang.jubging.project_backend.service.member.request;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
+@RequiredArgsConstructor
 public class RegionRequest {
 
     @NotNull
@@ -15,9 +17,4 @@ public class RegionRequest {
     @NotNull
     @Range(min = 33, max = 39, message = "latitude(위도)는 대한민국 영토 범위 33~39 내의 값이어야 합니다.")
     private final Double latitude;
-
-    public RegionRequest(Double longitude, Double latitude) {
-        this.longitude = longitude;
-        this.latitude = latitude;
-    }
 }
