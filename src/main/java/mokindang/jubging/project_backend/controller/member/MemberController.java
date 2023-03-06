b91b85d738fcf87298abb22af1d709cba01c78c6
최지환 <cjh87467@gmail.com>
2023-03-06 23:32:03 +0900

    refactor : #129 유저 지역 업데이트 api 를 MemberController 로 이전

---
+++ src/main/java/mokindang/jubging/project_backend/controller/member/MemberController.java
@@ -0,0 +1,30 @@
+package mokindang.jubging.project_backend.controller.member;
+
+import lombok.RequiredArgsConstructor;
+import mokindang.jubging.project_backend.domain.region.vo.Region;
+import mokindang.jubging.project_backend.service.member.MemberService;
+import mokindang.jubging.project_backend.service.member.request.RegionUpdateRequest;
+import mokindang.jubging.project_backend.service.member.response.RegionUpdateResponse;
+import mokindang.jubging.project_backend.web.argumentresolver.Login;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PutMapping("/region")
+    public ResponseEntity<RegionUpdateResponse> updateRegion(@Login Long memberId, @Valid @RequestBody RegionUpdateRequest regionUpdateRequest) {
+        Region updateRegion = memberService.updateRegion(regionUpdateRequest, memberId);
+        return ResponseEntity.ok()
+                .body(new RegionUpdateResponse(updateRegion.getValue()));
+    }
+}
