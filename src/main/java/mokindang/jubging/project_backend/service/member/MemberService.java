677c5643c2ab502ecabf6a011f957c867e8553d7
최지환 <cjh87467@gmail.com>
2023-03-06 23:21:08 +0900

    refactor : #129 유저 지역 인증 로직 MemberService 로 이전

--- src/main/java/mokindang/jubging/project_backend/service/member/MemberService.java
+++ src/main/java/mokindang/jubging/project_backend/service/member/MemberService.java
@@ -2,7 +2,10 @@ package mokindang.jubging.project_backend.service.member;
 
 import lombok.RequiredArgsConstructor;
 import mokindang.jubging.project_backend.domain.member.Member;
+import mokindang.jubging.project_backend.domain.region.vo.Region;
 import mokindang.jubging.project_backend.repository.member.MemberRepository;
+import mokindang.jubging.project_backend.security.kakao.KaKaoLocal;
+import mokindang.jubging.project_backend.service.member.request.RegionUpdateRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,11 +14,13 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final KaKaoLocal kakaoLocalApi;
     private final MemberRepository memberRepository;
 
     public Member saveMember(Member member) {
         return memberRepository.save(member);
     }
+
     public Optional<Member> findByMemberEmail(String memberEmail) {
         return memberRepository.findByEmail(memberEmail);
     }
@@ -23,5 +28,11 @@ public class MemberService {
     public Member findByMemberId(Long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalArgumentException("해당하는 유저가 존재하지 않습니다."));
+    }
+
+    public Region updateRegion(final RegionUpdateRequest regionUpdateRequest, final Long memberId) {
+        Member member = findByMemberId(memberId);
+        member.updateRegion(kakaoLocalApi.switchCoordinateToRegion(regionUpdateRequest));
+        return member.getRegion();
     }
 }
