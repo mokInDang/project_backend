3d7977a6e829d711010d22f1eab2b1c7499a9533
mino <koho104777@gmail.com>
2023-03-06 16:43:13 +0900

    feat : #130 Access Token Type Bearer 구현

--- src/main/java/mokindang/jubging/project_backend/web/jwt/TokenManager.java
+++ src/main/java/mokindang/jubging/project_backend/web/jwt/TokenManager.java
@@ -13,6 +13,8 @@ import java.util.Date;
 public class TokenManager {
 
     private static final String ISSUER = "mokindang";
+    private static final String MEMBER_ID = "memberId";
+    private static final String TOKEN_TYPE = "Bearer ";
     private final String secretKey;
     private final Long tokenValidateTime;
 
@@ -25,29 +27,31 @@ public class TokenManager {
     public String createToken(Long memberId) {
         Date now = new Date();
         generateKey();
-        return Jwts.builder()
+        String token = Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-                .claim("memberId", memberId)
+                .claim(MEMBER_ID, memberId)
                 .setIssuer(ISSUER)
                 .setIssuedAt(now)
                 .signWith(generateKey(), SignatureAlgorithm.HS256)
                 .setExpiration(new Date(now.getTime() + Duration.ofMinutes(tokenValidateTime).toMillis()))
                 .compact();
+        return TOKEN_TYPE + token;
     }
 
     public Long getMemberId(String token) {
+        String accessToken = parseTokenType(token);
         return Jwts.parserBuilder()
                 .setSigningKey(generateKey())
                 .build()
-                .parseClaimsJws(token)
+                .parseClaimsJws(accessToken)
                 .getBody()
-                .get("memberId",Long.class);
+                .get(MEMBER_ID,Long.class);
     }
-
 
     public boolean validateToken(final String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(generateKey()).build().parseClaimsJws(token);
+            String accessToken = parseTokenType(token);
+            Jwts.parserBuilder().setSigningKey(generateKey()).build().parseClaimsJws(accessToken);
             return true;
         } catch (final io.jsonwebtoken.security.SignatureException | SecurityException | MalformedJwtException exception) {
             throw new JwtException("토큰이 유효하지 않습니다.");
@@ -57,7 +61,13 @@ public class TokenManager {
             throw new JwtException("지원하지 않는 토큰입니다.");
         } catch (final IllegalArgumentException exception) {
             throw new JwtException("JWT 토큰이 잘못되었습니다.");
+        } catch (final ArrayIndexOutOfBoundsException exception) {
+            throw new JwtException("Token 타입이 명시되지 않았습니다.");
         }
+    }
+
+    private String parseTokenType(String token) {
+        return token.split(" ")[1];
     }
 
     private Key generateKey() {
