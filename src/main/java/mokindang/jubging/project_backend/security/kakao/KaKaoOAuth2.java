9e5b9cfc31f56b6e3842c72f3e1a40dd60175799
mino <koho104777@gmail.com>
2023-03-07 19:04:44 +0900

    refactor : redirect url 수정

--- src/main/java/mokindang/jubging/project_backend/security/kakao/KaKaoOAuth2.java
+++ src/main/java/mokindang/jubging/project_backend/security/kakao/KaKaoOAuth2.java
@@ -30,7 +30,7 @@ public class KaKaoOAuth2 {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", "60b35611c843f6c8f618a495ecc8eaf6");
-        params.add("redirect_uri", "http://localhost:8080/api/member/join");
+        params.add("redirect_uri", "https://www.dongnejupging.xyz/api/member/join");
         params.add("code", authorizationCode);
 
         RestTemplate rt = new RestTemplate();
