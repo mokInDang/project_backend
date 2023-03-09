ddf0356c69a735198879a6c659d54f999f46e7f1
최지환 <cjh87467@gmail.com>
2023-03-09 23:11:04 +0900

    feat : #158 게시글 조회 메서드 구현

--- src/main/java/mokindang/jubging/project_backend/service/board/BoardService.java
+++ src/main/java/mokindang/jubging/project_backend/service/board/BoardService.java
@@ -5,6 +5,7 @@ import mokindang.jubging.project_backend.domain.board.Board;
 import mokindang.jubging.project_backend.domain.member.Member;
 import mokindang.jubging.project_backend.repository.board.BoardRepository;
 import mokindang.jubging.project_backend.service.board.request.BoardCreateRequest;
+import mokindang.jubging.project_backend.service.board.response.BoardSelectResponse;
 import mokindang.jubging.project_backend.service.member.MemberService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +24,14 @@ public class BoardService {
         Board board = new Board(member, boardCreateRequest.getStartingDate(), boardCreateRequest.getActivityCategory(),
                 boardCreateRequest.getTitle(), boardCreateRequest.getContent(), boardCreateRequest.getRequestDate());
         boardRepository.save(board);
+    }
+
+    public BoardSelectResponse select(final Long memberId, final Long boardId) {
+        Member logindMember = memberService.findByMemberId(memberId);
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물입니다."));
+        board.checkRegion(logindMember.getRegion());
+        return new BoardSelectResponse(board.getId(), board.getTitle().getValue(), board.getContent().getValue(), board.getWriter().getAlias(),
+                board.getStartingDate(),board.getRegion(), board.getActivityCategory(), board.isOnRecruitment());
     }
 }
