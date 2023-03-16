f31cf5107a526f0a1ef76a7941cee0b5bee8b36c
최지환 <cjh87467@gmail.com>
2023-03-17 00:03:02 +0900

    refactor : #191 로깅 설정 추가

--- src/main/java/mokindang/jubging/project_backend/scheduler/BoardScheduler.java
+++ src/main/java/mokindang/jubging/project_backend/scheduler/BoardScheduler.java
@@ -1,10 +1,14 @@
 package mokindang.jubging.project_backend.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mokindang.jubging.project_backend.service.board.BoardSchedulingService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BoardScheduler {
@@ -15,6 +19,7 @@ public class BoardScheduler {
 
     @Scheduled(cron = EVERY_MIDNIGHT)
     public void updateOnRecruitment() {
+        log.info("매일 자정 게시글 모집여부 업데이트 스케줄러 실행, 실행 시각 = {}", LocalDateTime.now());
         boardSchedulingService.updateOnRecruitmentByStartingDate();
     }
 }
