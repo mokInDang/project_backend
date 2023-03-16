4068732da71f5b381cbf20a570954a35004dee04
최지환 <cjh87467@gmail.com>
2023-03-16 22:05:22 +0900

    feat : #191 findBoardsByOnRecruitmentTrue 메서드 작성

--- src/main/java/mokindang/jubging/project_backend/repository/board/BoardRepository.java
+++ src/main/java/mokindang/jubging/project_backend/repository/board/BoardRepository.java
@@ -3,9 +3,12 @@ package mokindang.jubging.project_backend.repository.board;
 import mokindang.jubging.project_backend.domain.board.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Optional<Board> findById(Long id);
+
+    List<Board> findBoardsByOnRecruitmentTrue();
 }
