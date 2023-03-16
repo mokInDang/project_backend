a0981edded15365c2bf527af623f4c9a702c374f
최지환 <cjh87467@gmail.com>
2023-03-16 13:35:59 +0900

    test : #191 활동 시작일 ifBefore 테스트 작성

--- src/test/java/mokindang/jubging/project_backend/domain/board/vo/StartingDateTest.java
+++ src/test/java/mokindang/jubging/project_backend/domain/board/vo/StartingDateTest.java
@@ -9,8 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.time.LocalDate;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 class StartingDateTest {
 
@@ -39,5 +38,26 @@ class StartingDateTest {
         //when,then
         assertThatThrownBy(() -> new StartingDate(current, pastStartingDate)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미 지난 날짜는 활동 시작일로 할 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("localDateInputsProvider")
+    @DisplayName("활동 시작일이 입력받은 시간보다 이전에 있으면 true 를 반환한다. 그렇지 않은경우 false 반환")
+    void ifBefore(final LocalDate input, final boolean expect) {
+        //given
+        StartingDate startingDate = new StartingDate(current, LocalDate.of(2023, 3, 16));
+
+        //when
+        boolean actual = startingDate.isBefore(input);
+
+        //then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    private static Stream<Arguments> localDateInputsProvider() {
+        return Stream.of(Arguments.of(LocalDate.of(2023, 3, 15), false),
+                Arguments.of(LocalDate.of(2023, 3, 16), false),
+                Arguments.of(LocalDate.of(2023, 3, 17), true)
+        );
     }
 }
