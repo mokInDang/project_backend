daa380963d9c79dfc214e0d979d794094f2ef306
최지환 <cjh87467@gmail.com>
2023-03-23 23:54:38 +0900

    refactor : #209 게시글 작성 후, 작성 게시글 번호 반환 하도록 함

---
+++ src/main/java/mokindang/jubging/project_backend/service/board/response/BoardIdResponse.java
@@ -0,0 +1,11 @@
+package mokindang.jubging.project_backend.service.board.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BoardIdResponse {
+
+    private Long boardId;
+}
