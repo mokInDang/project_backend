2bda1047f162dd50882788e7132780c76a7d34ff
최지환 <cjh87467@gmail.com>
2023-03-30 10:53:40 +0900

    refactor : #210 스웨거 스키마 설정 추가

--- src/main/java/mokindang/jubging/project_backend/service/board/response/MultiBoardSelectResponse.java
+++ src/main/java/mokindang/jubging/project_backend/service/board/response/MultiBoardSelectResponse.java
@@ -1,5 +1,6 @@
 package mokindang.jubging.project_backend.service.board.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,9 @@ import java.util.List;
 @Getter
 public class MultiBoardSelectResponse {
 
+    @Schema(description = "조회된 요약 게시글 목록")
     private final List<SummaryBoardResponse> boards;
+
+    @Schema(description = "다음 게시글 존재 여부")
     private final boolean hasNext;
 }
