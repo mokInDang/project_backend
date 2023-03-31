3b38cae7ca7ab881a99dfea8114c3422941c8ea5
mino <koho104777@gmail.com>
2023-03-31 20:12:21 +0900

    feature : #207 ProfileImage vo 구현

---
+++ src/main/java/mokindang/jubging/project_backend/domain/member/vo/ProfileImage.java
@@ -0,0 +1,37 @@
+package mokindang.jubging.project_backend.domain.member.vo;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ProfileImage {
+
+    public static final String DEFAULT_PROFILE_IMAGE_URL = "DEFAULT_PROFILE_IMAGE_URL";
+    public static final String DEFAULT_PROFILE_IMAGE_NAME = "DEFAULT_PROFILE_IMAGE_NAME";
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "profile_image_name")
+    private String profileImageName;
+
+    public ProfileImage(String profileImageUrl, String profileImageName) {
+        this.profileImageUrl = profileImageUrl;
+        this.profileImageName = profileImageName;
+    }
+
+    public static ProfileImage createByNull() {
+        return new ProfileImage(DEFAULT_PROFILE_IMAGE_URL, DEFAULT_PROFILE_IMAGE_NAME);
+    }
+
+    public void updateProfileImage(final String profileImageUrl, final String profileImageName) {
+        this.profileImageUrl = profileImageUrl;
+        this.profileImageName = profileImageName;
+    }
+}
