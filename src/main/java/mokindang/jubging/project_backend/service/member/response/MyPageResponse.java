54f74771f1f6cb0e990ed2ffabb29347d434f5b4
mino <koho104777@gmail.com>
2023-04-04 15:36:08 +0900

    feature : #223 MyPageResponse 스웨거 적용

--- src/main/java/mokindang/jubging/project_backend/service/member/response/MyPageResponse.java
+++ src/main/java/mokindang/jubging/project_backend/service/member/response/MyPageResponse.java
@@ -1,5 +1,6 @@
 package mokindang.jubging.project_backend.service.member.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,9 +10,12 @@ import lombok.RequiredArgsConstructor;
 @Builder
 public class MyPageResponse {
 
+    @Schema(description = "마이페이지 닉네임", example = "민호")
     private final String alias;
 
+    @Schema(description = "마이페이지 지역명", example = "부천시")
     private final String region;
 
+    @Schema(description = "마이페이지 프로필 URL", example = "https://test.png")
     private final String profileImageUrl;
 }
