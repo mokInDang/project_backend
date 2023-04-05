cfcecace25c28c8782a6be3da6e0a5ba3d819cf0
mino <koho104777@gmail.com>
2023-04-05 22:30:38 +0900

    refactor : #207 업데이트 부분 서비스 레이어로 분리

--- src/main/java/mokindang/jubging/project_backend/controller/member/MemberController.java
+++ src/main/java/mokindang/jubging/project_backend/controller/member/MemberController.java
@@ -2,7 +2,6 @@ package mokindang.jubging.project_backend.controller.member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import mokindang.jubging.project_backend.domain.member.Member;
 import mokindang.jubging.project_backend.domain.member.vo.Region;
 import mokindang.jubging.project_backend.service.file.FileResponse;
 import mokindang.jubging.project_backend.service.file.FileService;
@@ -17,13 +16,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 
-import static mokindang.jubging.project_backend.domain.member.vo.ProfileImage.DEFAULT_PROFILE_IMAGE_NAME;
-import static mokindang.jubging.project_backend.domain.member.vo.ProfileImage.DEFAULT_PROFILE_IMAGE_URL;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
-@Slf4j
 public class MemberController implements MemberControllerSwagger{
 
     private final MemberService memberService;
@@ -45,18 +41,8 @@ public class MemberController implements MemberControllerSwagger{
 
     @PostMapping("/upload")
     public ResponseEntity<FileResponse> uploadProfileImage(@Login Long memberId, @RequestPart(value = "file") MultipartFile multipartFile) {
-        Member member = memberService.findByMemberId(memberId);
-
-        FileResponse fileResponse = fileService.uploadFile(multipartFile);
-        log.info("memberId = {}, alias = {} 의 프로필 이미지 {} 업로드", member.getId(), member.getAlias(), fileResponse.getUploadFileName());
-
-        if (!member.getProfileImage().getProfileImageUrl().equals(DEFAULT_PROFILE_IMAGE_URL) && !member.getProfileImage().getProfileImageName().equals(DEFAULT_PROFILE_IMAGE_NAME)) {
-            fileService.deleteFile(member.getProfileImage().getProfileImageName());
-            log.info("memberId = {}, alias = {} 의 이전 프로필 이미지 {} 삭제", member.getId(), member.getAlias(), member.getProfileImage().getProfileImageName());
-        }
-
-        memberService.updateProfileImage(member, fileResponse.getUploadFileUrl(), fileResponse.getUploadFileName());
-
+        FileResponse fileResponse = fileService.uploadFile(multipartFile, memberId);
+        fileService.deleteFile(memberId);
         return ResponseEntity.ok()
                 .body(fileResponse);
     }
