302883523b602e4bd5da008e7f0f62394f3ba493
최지환 <cjh87467@gmail.com>
2023-04-05 20:27:36 +0900

    feat : #277 게시글 수정 기능 구현

--- src/main/java/mokindang/jubging/project_backend/service/board/BoardService.java
+++ src/main/java/mokindang/jubging/project_backend/service/board/BoardService.java
@@ -5,9 +5,10 @@ import mokindang.jubging.project_backend.domain.board.Board;
 import mokindang.jubging.project_backend.domain.member.Member;
 import mokindang.jubging.project_backend.exception.custom.ForbiddenException;
 import mokindang.jubging.project_backend.repository.board.BoardRepository;
-import mokindang.jubging.project_backend.service.board.request.BoardCreateRequest;
+import mokindang.jubging.project_backend.service.board.request.BoardCreationRequest;
+import mokindang.jubging.project_backend.service.board.request.BoardModificationRequest;
 import mokindang.jubging.project_backend.service.board.response.BoardIdResponse;
-import mokindang.jubging.project_backend.service.board.response.BoardSelectResponse;
+import mokindang.jubging.project_backend.service.board.response.BoardSelectionResponse;
 import mokindang.jubging.project_backend.service.board.response.MultiBoardSelectResponse;
 import mokindang.jubging.project_backend.service.board.response.SummaryBoardResponse;
 import mokindang.jubging.project_backend.service.member.MemberService;
@@ -29,16 +30,16 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     @Transactional
-    public BoardIdResponse write(final Long memberId, final BoardCreateRequest boardCreateRequest) {
+    public BoardIdResponse write(final Long memberId, final BoardCreationRequest boardCreationRequest) {
         Member member = memberService.findByMemberId(memberId);
         LocalDateTime now = LocalDateTime.now();
-        Board board = new Board(now, member, boardCreateRequest.getStartingDate(), boardCreateRequest.getActivityCategory(),
-                boardCreateRequest.getTitle(), boardCreateRequest.getContent());
+        Board board = new Board(now, member, boardCreationRequest.getStartingDate(), boardCreationRequest.getActivityCategory(),
+                boardCreationRequest.getTitle(), boardCreationRequest.getContent());
         Board savedBoard = boardRepository.save(board);
         return new BoardIdResponse(savedBoard.getId());
     }
 
-    public BoardSelectResponse select(final Long memberId, final Long boardId) {
+    public BoardSelectionResponse select(final Long memberId, final Long boardId) {
         Member loggedInMember = memberService.findByMemberId(memberId);
         Board board = boardRepository.findById(boardId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물입니다."));
@@ -46,8 +47,8 @@ public class BoardService {
         return convertToBoardSelectResponse(loggedInMember, board);
     }
 
-    private BoardSelectResponse convertToBoardSelectResponse(final Member logindMember, final Board board) {
-        return new BoardSelectResponse(board.getId(), board.getTitle().getValue(), board.getContent().getValue(),
+    private BoardSelectionResponse convertToBoardSelectResponse(final Member logindMember, final Board board) {
+        return new BoardSelectionResponse(board.getId(), board.getTitle().getValue(), board.getContent().getValue(),
                 board.getCreatingDateTime(), board.getWriter().getAlias(), board.getStartingDate().getValue(),
                 board.getWritingRegion().getValue(), board.getActivityCategory().getValue(), board.isOnRecruitment(),
                 board.getWriter().getFourLengthEmail(), board.isWriter(logindMember));
@@ -72,10 +73,25 @@ public class BoardService {
         Member loggedInMember = memberService.findByMemberId(memberId);
         Board board = boardRepository.findById(boardId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물입니다."));
-        if (!board.isWriter(loggedInMember)) {
-            throw new ForbiddenException("글 작성자만 게시글을 삭제할 수 있습니다.");
-        }
+        validatePermission(board, loggedInMember, "글 작성자만 게시글을 삭제할 수 있습니다.");
         boardRepository.delete(board);
         return new BoardIdResponse(board.getId());
+    }
+
+    private static void validatePermission(final Board board, final Member loggedInMember, final String message) {
+        if (!board.isWriter(loggedInMember)) {
+            throw new ForbiddenException(message);
+        }
+    }
+
+    @Transactional
+    public BoardIdResponse modifiy(final Long memberId, final Long boardId, final BoardModificationRequest boardModificationRequest) {
+        Member loggedInMember = memberService.findByMemberId(memberId);
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물입니다."));
+        validatePermission(board, loggedInMember, "글 작성자만 게시글을 수정할 수 있습니다.");
+        board.modify(boardModificationRequest.getStartingDate(), boardModificationRequest.getActivityCategory(),
+                boardModificationRequest.getTitle(),boardModificationRequest.getContent());
+        return new BoardIdResponse(boardId);
     }
 }
