ed9fe5a319fc593a73550d349b205b5babddcd8a
최지환 <cjh87467@gmail.com>
2023-04-06 13:43:40 +0900

    docs : #245 스웨거 적용

--- src/main/java/mokindang/jubging/project_backend/controller/board/BoardControllerSwagger.java
+++ src/main/java/mokindang/jubging/project_backend/controller/board/BoardControllerSwagger.java
@@ -10,14 +10,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import mokindang.jubging.project_backend.exception.ErrorResponse;
 import mokindang.jubging.project_backend.service.board.request.BoardCreationRequest;
+import mokindang.jubging.project_backend.service.board.request.BoardModificationRequest;
 import mokindang.jubging.project_backend.service.board.response.BoardIdResponse;
 import mokindang.jubging.project_backend.service.board.response.BoardSelectionResponse;
+import mokindang.jubging.project_backend.service.board.response.MultiBoardSelectResponse;
 import mokindang.jubging.project_backend.web.argumentresolver.Login;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -50,8 +50,46 @@ public interface BoardControllerSwagger {
             @ApiResponse(responseCode = "200", description = "글 조회"),
             @ApiResponse(responseCode = "400", description = "지역이 유효하지 않는 유저 \t\n" +
                     "존재하지 않는 게시글 조회", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-            }
+    }
     )
     @GetMapping("/{boardId}")
     ResponseEntity<BoardSelectionResponse> selectBoard(@Parameter(hidden = true) @Login Long memberId, @PathVariable final Long boardId);
+
+    @Operation(description = "전체 게시글 리스트 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "전체 게시글 리스트 조회 완료"),
+    })
+    @GetMapping
+    ResponseEntity<MultiBoardSelectResponse> selectBoards(final Pageable pageable);
+
+    @Operation(summary = "게시글 삭제", parameters = {
+            @Parameter(name = "boardId", description = "삭제할 Board 의 Id", in = ParameterIn.PATH),
+            @Parameter(name = AUTHORIZATION, description = "access token", in = ParameterIn.HEADER, required = true),
+            @Parameter(name = SET_COOKIE, description = "refreshToken", in = ParameterIn.COOKIE, required = true)})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시글 삭제 완료",
+                    content = @Content(schema = @Schema(implementation = BoardIdResponse.class))),
+            @ApiResponse(responseCode = "400", description = "존재하지 않는 게시글에 대한 삭제 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "삭제 권한이 없는 회원의 삭제 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @DeleteMapping("/{boardId}")
+    ResponseEntity<BoardIdResponse> delete(@Parameter(hidden = true) @Login final Long memberId, @PathVariable final Long boardId);
+
+    @Operation(summary = "게시글 수정", parameters = {
+            @Parameter(name = "boardId", description = "수정할 Board 의 Id", in = ParameterIn.PATH),
+            @Parameter(name = AUTHORIZATION, description = "access token", in = ParameterIn.HEADER, required = true),
+            @Parameter(name = SET_COOKIE, description = "refreshToken", in = ParameterIn.COOKIE, required = true)})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시글 수정 완료",
+                    content = @Content(schema = @Schema(implementation = BoardIdResponse.class))),
+            @ApiResponse(responseCode = "400", description = "존재하지 않는 게시글에 대한 수정 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = " 권한이 없는 회원의 수정 요청",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PatchMapping("/{boardId}")
+    ResponseEntity<BoardIdResponse> modifyBoard(@Parameter(hidden = true) @Login final Long memberId, @PathVariable final Long boardId,
+                                                @RequestBody final BoardModificationRequest modificationRequest);
 }
