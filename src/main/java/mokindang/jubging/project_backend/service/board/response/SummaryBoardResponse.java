34f7c989cd2e90177e340e72c1d0850af681a711
최지환 <cjh87467@gmail.com>
2023-04-14 00:12:12 +0900

    refactor : #279 생성자 추가 및 기존 생성자 제거

--- src/main/java/mokindang/jubging/project_backend/service/board/response/SummaryBoardResponse.java
+++ src/main/java/mokindang/jubging/project_backend/service/board/response/SummaryBoardResponse.java
@@ -1,40 +1,57 @@
 package mokindang.jubging.project_backend.service.board.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import mokindang.jubging.project_backend.domain.board.Board;
 
 @Getter
-@AllArgsConstructor
 public class SummaryBoardResponse {
 
     @Schema(description = "게시글 번호", example = "1")
-    private Long boardId;
+    private final Long boardId;
 
     @Schema(description = "게시글 제목", example = "예시 제목 입니다.")
-    private String title;
+    private final String title;
 
     @Schema(description = "게시글 본문", example = "게시글 본문 입니다.")
-    private String content;
+    private final String content;
 
     @Schema(description = "작성자", example = "작성자닉네임")
-    private String writerAlias;
+    private final String writerAlias;
 
     @Schema(description = "작성자 프로필 url")
-    private String writerProfileUrl;
+    private final String writerProfileUrl;
 
     @Schema(description = "활동 예정일", example = "2023-12-11")
-    private String startingDate;
+    private final String startingDate;
 
     @Schema(description = "지역", example = "동작구")
-    private String region;
+    private final String region;
 
     @Schema(description = "활동 종류", example = "달리기", allowableValues = {"달리기", "산책"})
-    private String activityCategory;
+    private final String activityCategory;
 
     @Schema(description = "모집 여부")
-    private boolean onRecruitment;
+    private final boolean onRecruitment;
 
     @Schema(description = "게시글 작성자의 이메일 앞 4글자")
-    private String firstFourLettersOfEmail;
+    private final String firstFourLettersOfEmail;
+
+    public SummaryBoardResponse(final Board board) {
+        this.boardId = board.getId();
+        this.title = board.getTitle()
+                .getValue();
+        this.content = board.getContent()
+                .getValue();
+        this.writerAlias = board.getWriterAlias();
+        this.writerProfileUrl = board.getWriterProfileImageUrl();
+        this.startingDate = board.getStartingDate()
+                .getValue();
+        this.region = board.getWritingRegion()
+                .getValue();
+        this.activityCategory = board.getActivityCategory()
+                .getValue();
+        this.onRecruitment = board.isOnRecruitment();
+        this.firstFourLettersOfEmail = board.getFirstFourDigitsOfWriterEmail();
+    }
 }
