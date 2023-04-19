854d7424cad16f79ade0bac072d15374d27e302c
최지환 <cjh87467@gmail.com>
2023-04-20 01:32:40 +0900

    feat : #300 CommentCreationRequest 구현

---
+++ src/main/java/mokindang/jubging/project_backend/comment/service/request/CommentCreationRequest.java
@@ -0,0 +1,13 @@
+package mokindang.jubging.project_backend.comment.service.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentCreationRequest {
+
+    private String commentBody;
+}
