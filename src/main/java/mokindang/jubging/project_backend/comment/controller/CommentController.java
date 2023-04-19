f147960e7aae7cfa16d7e741a0aa4f9145dfe9b6
최지환 <cjh87467@gmail.com>
2023-04-20 01:34:15 +0900

    feat : #300 CommentController 구현

---
+++ src/main/java/mokindang/jubging/project_backend/comment/controller/CommentController.java
@@ -0,0 +1,30 @@
+package mokindang.jubging.project_backend.comment.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mokindang.jubging.project_backend.comment.service.CommentService;
+import mokindang.jubging.project_backend.comment.service.request.CommentCreationRequest;
+import mokindang.jubging.project_backend.recruitment_board.service.response.RecruitmentBoardIdResponse;
+import mokindang.jubging.project_backend.web.argumentresolver.Login;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/recruitment-board/{boardId}/comments")
+    public ResponseEntity<RecruitmentBoardIdResponse> addCommentToRecruitmentBoard(@Login final Long memberId, @PathVariable final Long boardId,
+                                                                                   @Valid @RequestBody final CommentCreationRequest commentCreationRequest) {
+        RecruitmentBoardIdResponse recruitmentBoardIdResponse = commentService.addCommentToRecruitmentBoard(memberId, boardId, commentCreationRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(recruitmentBoardIdResponse);
+    }
+}
