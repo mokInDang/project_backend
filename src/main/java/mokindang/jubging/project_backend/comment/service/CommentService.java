d0c236762789e41643d0d0f47cf1023418b1714b
최지환 <cjh87467@gmail.com>
2023-04-23 01:57:13 +0900

    feat : #311 댓글 정보 반환 로직 구현

--- src/main/java/mokindang/jubging/project_backend/comment/service/CommentService.java
+++ src/main/java/mokindang/jubging/project_backend/comment/service/CommentService.java
@@ -8,6 +8,8 @@ import mokindang.jubging.project_backend.comment.domain.Comment;
 import mokindang.jubging.project_backend.comment.repository.CommentRepository;
 import mokindang.jubging.project_backend.comment.service.request.CommentCreationRequest;
 import mokindang.jubging.project_backend.comment.service.response.BoardIdResponse;
+import mokindang.jubging.project_backend.comment.service.response.CommentSelectionResponse;
+import mokindang.jubging.project_backend.comment.service.response.MultiCommentSelectionResponse;
 import mokindang.jubging.project_backend.member.domain.Member;
 import mokindang.jubging.project_backend.member.service.MemberService;
 import mokindang.jubging.project_backend.recruitment_board.domain.RecruitmentBoard;
@@ -16,6 +18,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -49,5 +53,25 @@ public class CommentService {
             return new BoardIdResponse(boardId);
         }
         throw new IllegalArgumentException("존재 하지 않는 게시판에 대한 접근입니다.");
+    }
+
+    @Transactional
+    public MultiCommentSelectionResponse selectComments(final Long memberId, final BoardType boardType, final Long boardId) {
+        if (boardType.equals(BoardType.RECRUITMENT_BOARD)) {
+            List<Comment> commentsByRecruitmentBoard = commentRepository.findCommentsByRecruitmentBoard(boardId);
+            return new MultiCommentSelectionResponse(convertToCommentSelectionResponse(memberId, commentsByRecruitmentBoard));
+        }
+
+        if (boardType.equals(BoardType.CERTIFICATION_BOARD)) {
+            List<Comment> commentsByCertificationBoard = commentRepository.findCommentsByCertificationBoard(boardId);
+            return new MultiCommentSelectionResponse(convertToCommentSelectionResponse(memberId, commentsByCertificationBoard));
+        }
+        throw new IllegalArgumentException("존재 하지 않는 게시판에 대한 접근입니다.");
+    }
+
+    private List<CommentSelectionResponse> convertToCommentSelectionResponse(final Long memberId, final List<Comment> commentsByRecruitmentBoard) {
+        return commentsByRecruitmentBoard.stream()
+                .map(comment -> new CommentSelectionResponse(comment, memberId))
+                .collect(Collectors.toUnmodifiableList());
     }
 }
