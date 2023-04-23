3763eaa4261efc62e924f82f2efe35946c5b2f4a
최지환 <cjh87467@gmail.com>
2023-04-23 17:04:43 +0900

    test : #311 테스트 코드 추가

--- src/test/java/mokindang/jubging/project_backend/comment/controller/CommentControllerTest.java
+++ src/test/java/mokindang/jubging/project_backend/comment/controller/CommentControllerTest.java
@@ -1,10 +1,14 @@
 package mokindang.jubging.project_backend.comment.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import mokindang.jubging.project_backend.comment.domain.Comment;
+import mokindang.jubging.project_backend.comment.domain.vo.CommentBody;
 import mokindang.jubging.project_backend.comment.service.BoardType;
 import mokindang.jubging.project_backend.comment.service.CommentService;
 import mokindang.jubging.project_backend.comment.service.request.CommentCreationRequest;
 import mokindang.jubging.project_backend.comment.service.response.BoardIdResponse;
+import mokindang.jubging.project_backend.comment.service.response.CommentSelectionResponse;
+import mokindang.jubging.project_backend.comment.service.response.MultiCommentSelectionResponse;
 import mokindang.jubging.project_backend.web.jwt.TokenManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,9 +19,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,8 +47,8 @@ class CommentControllerTest {
     private TokenManager tokenManager;
 
     @Test
-    @DisplayName("입력받은 boardId 에 해당하는 recruitmentBoard 에 새 댓글을 추가한다.")
-    void addCommentToRecruitmentBoard() throws Exception {
+    @DisplayName("입력받은 BoardType 과 boardId 에 해당하는  게시글에 새 댓글을 추가한다.")
+    void addComment() throws Exception {
         //given
         when(commentService.addComment(anyLong(), any(BoardType.class), anyLong(), any(CommentCreationRequest.class)))
                 .thenReturn(new BoardIdResponse(1L));
@@ -55,5 +64,43 @@ class CommentControllerTest {
         //then
         actual.andExpect(status().isCreated())
                 .andExpect(jsonPath("$.boardId").value(1L));
+    }
+
+    @Test
+    @DisplayName("입력받은 BoardType 과 boardId 에 해당하는 게시글에 새 댓글을 추가한다.")
+    void selectComments() throws Exception {
+        //given
+        CommentSelectionResponse commentSelectionResponse1 = new CommentSelectionResponse(createMockedComment(), 1L);
+        when(commentService.selectComments(anyLong(), any(BoardType.class), anyLong()))
+                .thenReturn(new MultiCommentSelectionResponse(List.of(commentSelectionResponse1)));
+
+
+        //when
+        ResultActions actual = mockMvc.perform(get("/api/{board-type}/{boardId}/comments",
+                "recruitment-board", 1L)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        actual.andExpect(status().isOk())
+                .andExpect(jsonPath("$.comments[0].commentId").value(1))
+                .andExpect(jsonPath("$.comments[0].commentBody").value("본문내용"))
+                .andExpect(jsonPath("$.comments[0].mine").value(true))
+                .andExpect(jsonPath("$.comments[0].createdDatetime").value("2023-11-11T11:11:01"))
+                .andExpect(jsonPath("$.comments[0].writerAlias").value("댓글작성자"))
+                .andExpect(jsonPath("$.comments[0].firstFourLettersOfEmail").value("test"))
+                .andExpect(jsonPath("$.comments[0].writerProfileImageUrl").value("test_url"));
+
+    }
+    private Comment createMockedComment() {
+
+        Comment comment = mock(Comment.class);
+        when(comment.getId()).thenReturn(1L);
+        when(comment.getCommentBody()).thenReturn(new CommentBody("본문내용"));
+        when(comment.isSameWriterId(any())).thenReturn(true);
+        when(comment.getCreatedDateTime()).thenReturn(LocalDateTime.of(2023, 11, 11, 11, 11, 1));
+        when(comment.getWriterAlias()).thenReturn("댓글작성자");
+        when(comment.getFirstFourDigitsOfWriterEmail()).thenReturn("test");
+        when(comment.getWriterProfileImageUrl()).thenReturn("test_url");
+        return comment;
     }
 }
