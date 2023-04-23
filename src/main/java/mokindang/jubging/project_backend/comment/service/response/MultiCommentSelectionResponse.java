9b630218fc7c4fa8368038a9caba6f4e2cd8e3d5
최지환 <cjh87467@gmail.com>
2023-04-23 17:07:10 +0900

    refactor : #311 사용하지 않는 생성자 제거

--- src/main/java/mokindang/jubging/project_backend/comment/service/response/MultiCommentSelectionResponse.java
+++ src/main/java/mokindang/jubging/project_backend/comment/service/response/MultiCommentSelectionResponse.java
@@ -3,12 +3,10 @@ package mokindang.jubging.project_backend.comment.service.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class MultiCommentSelectionResponse {
 
