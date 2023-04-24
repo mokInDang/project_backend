82ac256baed0faf3aafba2418bb54eb93eab55bd
최지환 <cjh87467@gmail.com>
2023-04-24 15:24:59 +0900

    refactor : #316 기본생성자 추가

--- src/main/java/mokindang/jubging/project_backend/comment/service/request/ReplyCommentCreationRequest.java
+++ src/main/java/mokindang/jubging/project_backend/comment/service/request/ReplyCommentCreationRequest.java
@@ -2,9 +2,11 @@ package mokindang.jubging.project_backend.comment.service.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReplyCommentCreationRequest {
 
     private String replyCommentBody;
