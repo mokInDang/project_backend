f56ea36d7720e76a9af495190f8f3f252c9629dc
최지환 <cjh87467@gmail.com>
2023-04-24 15:35:17 +0900

    refactor : #361 DTO 제약조건 추가

--- src/main/java/mokindang/jubging/project_backend/comment/service/request/ReplyCommentCreationRequest.java
+++ src/main/java/mokindang/jubging/project_backend/comment/service/request/ReplyCommentCreationRequest.java
@@ -1,13 +1,20 @@
 package mokindang.jubging.project_backend.comment.service.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReplyCommentCreationRequest {
 
+    @Schema(description = "대댓글 본문")
+    @NotEmpty
+    @NotNull
     private String replyCommentBody;
 }
