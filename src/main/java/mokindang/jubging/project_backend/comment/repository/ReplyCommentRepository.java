aaaa9345f7bdc90d1f79d66118d13a68590cedf1
최지환 <cjh87467@gmail.com>
2023-04-24 17:04:28 +0900

    refactor : #319 대댓글 조회 메서드 생성

--- src/main/java/mokindang/jubging/project_backend/comment/repository/ReplyCommentRepository.java
+++ src/main/java/mokindang/jubging/project_backend/comment/repository/ReplyCommentRepository.java
@@ -3,5 +3,9 @@ package mokindang.jubging.project_backend.comment.repository;
 import mokindang.jubging.project_backend.comment.domain.ReplyComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyCommentRepository extends JpaRepository<ReplyComment, Long> {
+
+    List<ReplyComment> findReplyCommentsByCommentId(final Long commendId);
 }
