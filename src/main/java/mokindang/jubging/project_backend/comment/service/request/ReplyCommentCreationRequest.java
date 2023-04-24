a2f3522b3bf3836a8cb8b0b9560535eb1596dd25
최지환 <cjh87467@gmail.com>
2023-04-24 14:53:21 +0900

    feat : #316 ReplyCommentCreationRequest 생성

---
+++ src/main/java/mokindang/jubging/project_backend/comment/service/request/ReplyCommentCreationRequest.java
@@ -0,0 +1,11 @@
+package mokindang.jubging.project_backend.comment.service.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReplyCommentCreationRequest {
+
+    private String replyCommentBody;
+}
