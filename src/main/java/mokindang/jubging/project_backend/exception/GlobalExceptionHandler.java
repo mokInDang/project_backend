0352c2c77ff4860087fa0331bf8af2bb80540de7
최지환 <cjh87467@gmail.com>
2023-05-02 14:04:12 +0900

    refactor : #109 RuntimeException 에 대한 로깅 설정 추가

--- src/main/java/mokindang/jubging/project_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/mokindang/jubging/project_backend/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package mokindang.jubging.project_backend.exception;
 
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import mokindang.jubging.project_backend.exception.custom.ForbiddenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -18,6 +20,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(final RuntimeException e) {
+        log.warn(e.getMessage());
         return ResponseEntity.internalServerError()
                 .body(new ErrorResponse(e.getMessage()));
     }
