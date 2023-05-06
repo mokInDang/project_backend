4d8c290e1b0f55f2de0811d4c867195ff4c6b49b
mino <koho104777@gmail.com>
2023-05-06 15:11:27 +0900

    feature : #336 ImageService 파일 업로드, 삭제 기능 추가

--- src/main/java/mokindang/jubging/project_backend/image/service/ImageService.java
+++ src/main/java/mokindang/jubging/project_backend/image/service/ImageService.java
@@ -1,21 +1,33 @@
 package mokindang.jubging.project_backend.image.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mokindang.jubging.project_backend.certification_board.domain.CertificationBoard;
+import mokindang.jubging.project_backend.file.FileResponse;
+import mokindang.jubging.project_backend.file.FileService;
 import mokindang.jubging.project_backend.image.domain.Image;
 import mokindang.jubging.project_backend.image.repository.ImageRepository;
+import mokindang.jubging.project_backend.image.service.request.ProfileImageDeleteRequest;
+import mokindang.jubging.project_backend.image.service.request.ProfileImageRequest;
+import mokindang.jubging.project_backend.image.service.response.ProfileImageResponse;
+import mokindang.jubging.project_backend.member.domain.Member;
+import mokindang.jubging.project_backend.member.service.MemberService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ImageService {
 
+    private final String defaultImageUrl = "https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/default_profile.png";
     private final ImageRepository imageRepository;
+    private final FileService fileService;
+    private final MemberService memberService;
 
     public List<String> findImagesUrl(final CertificationBoard board) {
         List<String> imagesUrl = new ArrayList<>();
@@ -44,5 +56,21 @@ public class ImageService {
         for (Image image : images) {
             imagesName.add(image.getStoreName());
         }
+    }
+
+    public ProfileImageResponse uploadProfileImage(Long memberId, ProfileImageRequest profileImageRequest) {
+        Member member = memberService.findByMemberId(memberId);
+        FileResponse fileResponse = fileService.uploadFile(profileImageRequest.getProfileImage());
+        log.info("memberId = {}, alias = {} 의 프로필 이미지 {} 업로드", member.getId(), member.getAlias(), fileResponse.getUploadFileName());
+        return new ProfileImageResponse(fileResponse.getUploadFileUrl());
+    }
+
+    public ProfileImageResponse deleteProfileImage(ProfileImageDeleteRequest profileImageDeleteRequest) {
+        String profileImageUrl = profileImageDeleteRequest.getProfileImageUrl();
+        if (profileImageUrl.equals(defaultImageUrl)) {
+            return new ProfileImageResponse(defaultImageUrl);
+        }
+        fileService.deleteFile(profileImageUrl, "profile_image");
+        return new ProfileImageResponse(defaultImageUrl);
     }
 }
