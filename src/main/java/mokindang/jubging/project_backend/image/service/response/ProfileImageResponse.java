63f68a9614acf758e26e63ea669daf05f699f9ae
mino <koho104777@gmail.com>
2023-05-06 15:16:40 +0900

    feature : #336 이미지 url 분리 요청, 응답DTO 추가

---
+++ src/main/java/mokindang/jubging/project_backend/image/service/response/ProfileImageResponse.java
@@ -0,0 +1,13 @@
+package mokindang.jubging.project_backend.image.service.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileImageResponse {
+
+    private String profileImageUrl;
+}
