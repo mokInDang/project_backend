eef56665b2f317d31d7eb7e97e59e8fa5fd04467
최지환 <cjh87467@gmail.com>
2023-05-10 14:15:05 +0900

    test : #341 Coordinate 구현

---
+++ src/main/java/mokindang/jubging/project_backend/recruitment_board/domain/vo/Coordinate.java
@@ -0,0 +1,49 @@
+package mokindang.jubging.project_backend.recruitment_board.domain.vo;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Coordinate {
+
+    @Column
+    private Longitude longitude;
+
+    @Column
+    private Latitude latitude;
+
+    public Coordinate(final Double longitude, final Double latitude) {
+        this.longitude = new Longitude(longitude);
+        this.latitude = new Latitude(latitude);
+    }
+
+    public Double getPointOfLongitude() {
+        return longitude.getPoint();
+    }
+
+    public Double getPointOfLatitude() {
+        return latitude.getPoint();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Coordinate that = (Coordinate) o;
+
+        if (!longitude.equals(that.longitude)) return false;
+        return latitude.equals(that.latitude);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = longitude.hashCode();
+        result = 31 * result + latitude.hashCode();
+        return result;
+    }
+}
