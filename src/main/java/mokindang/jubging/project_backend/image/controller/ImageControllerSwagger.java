b8e66c2f1acc0f0b48d49caa581a7f734deec474
mino <koho104777@gmail.com>
2023-05-11 19:01:34 +0900

    feature : #339 인증게시글 이미지 분리 스웨거 적용

--- src/main/java/mokindang/jubging/project_backend/image/controller/ImageControllerSwagger.java
+++ src/main/java/mokindang/jubging/project_backend/image/controller/ImageControllerSwagger.java
@@ -6,9 +6,9 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import mokindang.jubging.project_backend.image.service.request.ProfileImageDeleteRequest;
-import mokindang.jubging.project_backend.image.service.request.ProfileImageRequest;
-import mokindang.jubging.project_backend.image.service.response.ProfileImageResponse;
+import mokindang.jubging.project_backend.image.service.request.ImageDeleteRequest;
+import mokindang.jubging.project_backend.image.service.request.ImageRequest;
+import mokindang.jubging.project_backend.image.service.response.ImageUrlResponse;
 import mokindang.jubging.project_backend.web.argumentresolver.Login;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,14 +21,14 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 @Tag(name = "이미지", description = "이미지 관련 api")
 public interface ImageControllerSwagger {
 
-    @Operation(summary = "프로필 이미지 수정", parameters = {
+    @Operation(summary = "프로필 이미지 추가", parameters = {
             @Parameter(name = AUTHORIZATION, description = "access token", in = ParameterIn.HEADER, required = true)}
     )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "변경완료")
     })
-    @PostMapping("profile-image")
-    ResponseEntity<ProfileImageResponse> uploadProfileImage(@Parameter(hidden = true) @Login long memberId, @ModelAttribute ProfileImageRequest profileImageRequest);
+    @PostMapping("profile")
+    ResponseEntity<ImageUrlResponse> uploadProfileImage(@Parameter(hidden = true) @Login Long memberId, @ModelAttribute ImageRequest imageRequest);
 
     @Operation(summary = "프로필 이미지 삭제", parameters = {
             @Parameter(name = AUTHORIZATION, description = "access token", in = ParameterIn.HEADER, required = true)}
@@ -36,7 +36,25 @@ public interface ImageControllerSwagger {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "삭제완료")
     })
-    @DeleteMapping("profile-image")
-    ResponseEntity<ProfileImageResponse> deleteProfileImage(@Parameter(hidden = true) @RequestBody ProfileImageDeleteRequest profileImageDeleteRequest);
+    @DeleteMapping("profile")
+    ResponseEntity<ImageUrlResponse> deleteProfileImage(@Parameter(hidden = true) @RequestBody ImageDeleteRequest imageDeleteRequest);
+
+    @Operation(summary = "인증게시글 이미지 추가", parameters = {
+            @Parameter(name = AUTHORIZATION, description = "access token", in = ParameterIn.HEADER, required = true)}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "변경완료")
+    })
+    @PostMapping("certification")
+    ResponseEntity<ImageUrlResponse> uploadCertificationImage(@Parameter(hidden = true) @Login Long memberId, @ModelAttribute ImageRequest imageRequest);
+
+    @Operation(summary = "인증게시글 이미지 삭제", parameters = {
+            @Parameter(name = AUTHORIZATION, description = "access token", in = ParameterIn.HEADER, required = true)}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "삭제완료")
+    })
+    @DeleteMapping("certification")
+    ResponseEntity<Void> deleteCertificationImage(@Parameter(hidden = true) @RequestBody ImageDeleteRequest imageDeleteRequest);
 
 }
