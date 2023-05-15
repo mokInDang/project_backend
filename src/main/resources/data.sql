5b80e45a48e087a09fbe00d363ffac8045feba4b
mino <koho104777@gmail.com>
2023-05-15 14:45:34 +0900

    refactor : #346 회원 생성 시 기존 프로필 이미지 url 주입

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO member (EMAIL, ALIAS, REGION, PROFILE_IMAGE_URL)
-VALUES ('test@email.com', '테스트유저', '동작구', 'DEFAULT_PROFILE_IMAGE_URL');
+VALUES ('test@email.com', '테스트유저', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
 INSERT INTO recruitment_board (MEMBER_ID, CREATING_DATE_TIME, STARTING_DATE, ACTIVITY_CATEGORY, TITLE, CONTENT_BODY, REGION, ON_RECRUITMENT)
 VALUES (1,'2023-3-9-12-0', '2023-3-15', 'RUNNING', '제목', '본문내용', '동작구', false);
 INSERT INTO comment (body, created_date_time, last_modified_date_time, certification_board_id, recruitment_board_id, member_id)
