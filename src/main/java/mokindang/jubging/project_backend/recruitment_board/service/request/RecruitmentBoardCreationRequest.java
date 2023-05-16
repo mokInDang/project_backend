e94a554c5e6b9b98365d1d7b2a54690ba861f6f2
최지환 <cjh87467@gmail.com>
2023-05-16 15:35:30 +0900

    refactor : #348 게시글 수정 요청 리퀘스트 추가 및 변경에 따른 게시글 작성 리퀘스트 변경

--- src/main/java/mokindang/jubging/project_backend/recruitment_board/service/request/RecruitmentBoardCreationRequest.java
+++ src/main/java/mokindang/jubging/project_backend/recruitment_board/service/request/RecruitmentBoardCreationRequest.java
@@ -35,14 +35,6 @@ public class RecruitmentBoardCreationRequest {
     private final LocalDate startingDate;
 
     @NotNull
-    @Schema(description = "활동 장소 경도 좌표", example = "12.12312412")
-    private final Double longitude;
-
-    @NotNull
-    @Schema(description = "활동 장소 위도 좌표", example = "34.12312312")
-    private final Double latitude;
-
-    @NotNull
-    @Schema(description = "활동 장소 상세 주소", example = "서울시 동작구 상도동 1-1")
-    private final String meetingAddress;
+    @Schema(description = "활동 장소")
+    private final MeetingPlaceRequest meetingPlaceRequest;
 }
