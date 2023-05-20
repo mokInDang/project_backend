69f7f4285096914a8977de3bfbda289292b7831d
최지환 <cjh87467@gmail.com>
2023-05-20 15:24:11 +0900

    refactor : #350 게시글 마커 조회시 게시글 제목도 반환도록 변경

--- src/main/java/mokindang/jubging/project_backend/recruitment_board/service/response/BoardPlaceMarkerResponse.java
+++ src/main/java/mokindang/jubging/project_backend/recruitment_board/service/response/BoardPlaceMarkerResponse.java
@@ -1,17 +1,24 @@
 package mokindang.jubging.project_backend.recruitment_board.service.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import mokindang.jubging.project_backend.recruitment_board.domain.RecruitmentBoard;
 
 @Getter
 public class BoardPlaceMarkerResponse {
 
+    @Schema(description = "게시글 id")
     private final RecruitmentBoardIdResponse recruitmentBoardIdResponse;
 
+    @Schema(description = "게시글 장소 정보")
     private final MeetingPlaceResponse meetingPlaceResponse;
+
+    @Schema(description = "게시글 제목")
+    private final String title;
 
     public BoardPlaceMarkerResponse(final RecruitmentBoard recruitmentBoard) {
         recruitmentBoardIdResponse = new RecruitmentBoardIdResponse(recruitmentBoard.getId());
         meetingPlaceResponse = new MeetingPlaceResponse(recruitmentBoard);
+        title = recruitmentBoard.getTitle().getValue();
     }
 }
