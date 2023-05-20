8101326c69fd412e0001aec8a2a9604d0f351850
최지환 <cjh87467@gmail.com>
2023-05-20 13:21:08 +0900

    feat : #350 활동 시작일 기준 오름차순 정렬한 모집 중인 지역 게시글 조회 메서드 구현

--- src/main/java/mokindang/jubging/project_backend/recruitment_board/repository/RecruitmentBoardRepository.java
+++ src/main/java/mokindang/jubging/project_backend/recruitment_board/repository/RecruitmentBoardRepository.java
@@ -10,11 +10,15 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface RecruitmentBoardRepository extends JpaRepository<RecruitmentBoard, Long> {
 
     @Modifying
-    @Query("UPDATE RecruitmentBoard b SET b.onRecruitment = false WHERE b.onRecruitment IS true AND b.startingDate.startingDate < :today")
+    @Query("UPDATE RecruitmentBoard b " +
+            "SET b.onRecruitment = false" +
+            " WHERE b.onRecruitment IS true " +
+            "AND b.startingDate.startingDate < :today")
     void updateOnRecruitmentByStartingDate(LocalDate today);
 
     @BatchSize(size = 1000)
@@ -22,6 +26,18 @@ public interface RecruitmentBoardRepository extends JpaRepository<RecruitmentBoa
     Slice<RecruitmentBoard> selectBoards(final Pageable pageable);
 
     @BatchSize(size = 1000)
-    @Query("SELECT b FROM RecruitmentBoard b WHERE b.writingRegion = :region ORDER BY b.creatingDateTime DESC")
+    @Query("SELECT b " +
+            "FROM RecruitmentBoard b " +
+            "WHERE b.writingRegion = :region " +
+            "ORDER BY b.creatingDateTime DESC")
     Slice<RecruitmentBoard> selectRegionBoards(final Region region, final Pageable pageable);
+
+
+    @Query("SELECT b " +
+            "FROM RecruitmentBoard  b " +
+            "WHERE b.writingRegion = :region " +
+            "AND b.onRecruitment = true " +
+            "ORDER By b.startingDate.startingDate ASC")
+    List<RecruitmentBoard> selectRecruitmentRegionBoardsCloseToDeadline(final Region region, final Pageable pageable);
+    
 }
