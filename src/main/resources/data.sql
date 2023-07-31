023b6ab6b4a5b2a55d0add2e500f42898f946ad4
최지환 <cjh87467@gmail.com>
2023-07-31 18:06:26 +0900

    docs : #393 테스트 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,36 @@
 INSERT INTO member (member_id, EMAIL, ALIAS, REGION, PROFILE_IMAGE_URL)
-VALUES (1,'test@email.com', '테스트유저', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
+VALUES (1,'test@email.com', '테스트유저1', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
+
+INSERT INTO member (member_id, EMAIL, ALIAS, REGION, PROFILE_IMAGE_URL)
+VALUES (2,'test2@email.com', '테스트유저2', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
+
+INSERT INTO member (member_id, EMAIL, ALIAS, REGION, PROFILE_IMAGE_URL)
+VALUES (3,'test3@email.com', '테스트유저3', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
+
+INSERT INTO member (member_id, EMAIL, ALIAS, REGION, PROFILE_IMAGE_URL)
+VALUES (4,'test4@email.com', '테스트유저4', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
+
+INSERT INTO member (member_id, EMAIL, ALIAS, REGION, PROFILE_IMAGE_URL)
+VALUES (5,'test5@email.com', '테스트유저5', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
+
+INSERT INTO member (member_id, EMAIL, ALIAS, REGION, PROFILE_IMAGE_URL)
+VALUES (6,'test6@email.com', '테스트유저6', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
+
+INSERT INTO member (member_id, EMAIL, ALIAS, REGION, PROFILE_IMAGE_URL)
+VALUES (7,'test7@email.com', '테스트유저7', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
+
+INSERT INTO member (member_id, EMAIL, ALIAS, REGION, PROFILE_IMAGE_URL)
+VALUES (8,'test8@email.com', '테스트유저8', '동작구', 'https://dognejupging-xyz-image-bucket.s3.ap-northeast-2.amazonaws.com/profile_image/profileimage2.png');
+
+
+-- 게시글
+INSERT INTO recruitment_board (MEMBER_ID, CREATING_DATE_TIME, STARTING_DATE, ACTIVITY_CATEGORY, TITLE, CONTENT_BODY,
+                               REGION, ON_RECRUITMENT, longitude_point, latitude_point, address, count_of_participation, max_count_of_participation, version)
+VALUES (1, '2023-3-9-12-0', '2023-3-15', 'RUNNING', '제목', '본문내용', '동작구', true, 1.1, 1.2, '서울시 동작구 상도동 1-1',1,8, 0);
 
 INSERT INTO recruitment_board (MEMBER_ID, CREATING_DATE_TIME, STARTING_DATE, ACTIVITY_CATEGORY, TITLE, CONTENT_BODY,
-                               REGION, ON_RECRUITMENT, longitude_point, latitude_point, address, count_of_participation, max_count_of_participation)
-VALUES (1, '2023-3-9-12-0', '2023-3-15', 'RUNNING', '제목', '본문내용', '동작구', true, 1.1, 1.2, '서울시 동작구 상도동 1-1',1,8);
-
-INSERT INTO recruitment_board (MEMBER_ID, CREATING_DATE_TIME, STARTING_DATE, ACTIVITY_CATEGORY, TITLE, CONTENT_BODY,
-                               REGION, ON_RECRUITMENT, longitude_point, latitude_point, address, count_of_participation, max_count_of_participation)
-VALUES (1, '2023-3-9-12-0', '2023-3-16', 'RUNNING', '제목2', '본문내용2', '동작구', true, 1.3, 1.4, '서울시 동작구 상도동 1-2',1,8);
+                               REGION, ON_RECRUITMENT, longitude_point, latitude_point, address, count_of_participation, max_count_of_participation,version)
+VALUES (1, '2023-3-9-12-0', '2023-3-16', 'RUNNING', '제목2', '본문내용2', '동작구', true, 1.3, 1.4, '서울시 동작구 상도동 1-2',1,8,0);
 
 INSERT INTO comment (body, created_date_time, last_modified_date_time, certification_board_id, recruitment_board_id,
                      member_id)
