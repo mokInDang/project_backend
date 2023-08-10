c2304c4f5b4e35f1918eb61be7190f5d32ced303
최지환 <cjh87467@gmail.com>
2023-08-10 21:07:51 +0900

    docs : #393 테스트 데이터 추가 인증 게시글 테스트 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,12 +29,22 @@ INSERT INTO recruitment_board (MEMBER_ID, CREATING_DATE_TIME, STARTING_DATE, ACT
 VALUES (1, '2023-3-9-12-0', '2023-3-15', 'RUNNING', '제목', '본문내용', '동작구', true, 1.1, 1.2, '서울시 동작구 상도동 1-1',1,8, 1);
 
 INSERT INTO recruitment_board (MEMBER_ID, CREATING_DATE_TIME, STARTING_DATE, ACTIVITY_CATEGORY, TITLE, CONTENT_BODY,
-                               REGION, ON_RECRUITMENT, longitude_point, latitude_point, address, count_of_participation, max_count_of_participation,version)
+                               REGION, ON_RECRUITMENT, longitude_point, latitude_point, address, count_of_participation, max_count_of_participation, version)
 VALUES (1, '2023-3-9-12-0', '2023-3-16', 'RUNNING', '제목2', '본문내용2', '동작구', true, 1.3, 1.4, '서울시 동작구 상도동 1-2',1,8,1);
 
+INSERT INTO certification_board (MEMBER_ID, CREATED_DATE_TIME, modifiedtime, TITLE, CONTENT_BODY)
+VALUES (1, '2023-3-9-12-0', '2023-3-9-12-0', '인증제목1', '인증본문1');
+
+
+--댓글
 INSERT INTO comment (body, created_date_time, last_modified_date_time, certification_board_id, recruitment_board_id,
                      member_id)
 VALUES ('테스트 댓글', '2023-3-10-11-0', '2023-3-10-11-0', null, 1, 1);
+
+INSERT INTO comment (body, created_date_time, last_modified_date_time, certification_board_id, recruitment_board_id,
+                     member_id)
+VALUES ('테스트 댓글', '2023-3-10-11-0', '2023-3-10-11-0', 1, null, 1);
+
 
 --대댓글
 INSERT INTO reply_comment (reply_comment_id, created_date_time, last_modified_date_time, body, comment_id, member_id)
