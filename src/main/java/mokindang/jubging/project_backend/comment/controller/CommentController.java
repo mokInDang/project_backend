791687728979c3a78f93b1986ac770354499a751
최지환 <cjh87467@gmail.com>
2023-08-10 23:17:22 +0900

    refactor : #392 사용하지 않는 예외 캐치 제거

--- src/main/java/mokindang/jubging/project_backend/comment/controller/CommentController.java
+++ src/main/java/mokindang/jubging/project_backend/comment/controller/CommentController.java
@@ -9,7 +9,6 @@ import mokindang.jubging.project_backend.comment.service.request.ReplyCommentCre
 import mokindang.jubging.project_backend.comment.service.response.BoardIdResponse;
 import mokindang.jubging.project_backend.comment.service.response.CommentIdResponse;
 import mokindang.jubging.project_backend.comment.service.response.MultiCommentSelectionResponse;
-import mokindang.jubging.project_backend.exception.custom.ForbiddenException;
 import mokindang.jubging.project_backend.web.argumentresolver.Login;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -41,14 +40,9 @@ public class CommentController implements CommentControllerSwagger {
                                                                         @PathVariable("board-type") final BoardType boardType,
                                                                         @PathVariable final Long boardId) {
         log.info("memberId = {} 의 게시판 {} boardId = {} 에 대한 댓글 조회 요청", memberId, boardType.toString(), boardId);
-        try {
             MultiCommentSelectionResponse multiCommentSelectionResponse = commentService.selectComments(memberId, boardType, boardId);
             return ResponseEntity.ok()
                     .body(multiCommentSelectionResponse);
-        } catch (ForbiddenException e) {
-            return ResponseEntity.noContent()
-                    .build();
-        }
     }
 
     @DeleteMapping("/comments/{commentId}")
