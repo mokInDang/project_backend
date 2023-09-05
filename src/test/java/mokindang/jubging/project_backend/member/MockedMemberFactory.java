e0d48033e8a0b3b5b8fed767c108ee60e45c36f7
최지환 <cjh87467@gmail.com>
2023-09-05 16:53:22 +0900

    refactor : #407 member 모킹 시 profileImage 에 대한 모킹 추가

--- src/test/java/mokindang/jubging/project_backend/member/MockedMemberFactory.java
+++ src/test/java/mokindang/jubging/project_backend/member/MockedMemberFactory.java
@@ -1,6 +1,7 @@
 package mokindang.jubging.project_backend.member;
 
 import mokindang.jubging.project_backend.member.domain.Member;
+import mokindang.jubging.project_backend.member.domain.vo.ProfileImage;
 import mokindang.jubging.project_backend.member.domain.vo.Region;
 
 import static org.mockito.Mockito.mock;
@@ -12,6 +13,7 @@ public class MockedMemberFactory {
         Member member = mock(Member.class);
         when(member.getId()).thenReturn(memberId);
         when(member.getRegion()).thenReturn(Region.from("동작구"));
+        when(member.getProfileImage()).thenReturn(new ProfileImage("test_url"));
         return member;
     }
 }
