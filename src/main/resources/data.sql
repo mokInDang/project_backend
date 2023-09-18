f204fe1819c1dce75f661de929e318d9179b7eb1
최지환 <cjh87467@gmail.com>
2023-09-18 22:41:30 +0900

    docs : #411 테스트 데이터 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -65,12 +65,6 @@ VALUES ('테스트 댓글', '2023-3-10-11-0', '2023-3-10-11-0', 'RECRUITMENT_BOA
 INSERT INTO reply_comment (reply_comment_id, created_date_time, last_modified_date_time, body, comment_id, member_id)
 VALUES (1, '2023-3-10-11-0', '2023-3-10-11-0', '테스트 대댓글1', 1, 1);
 
-# INSERT INTO reply_comment (reply_comment_id, created_date_time, last_modified_date_time, body, comment_id, member_id)
-# VALUES (2, '2023-3-10-11-0', '2023-4-10-11-0', '테스트 대댓글2', 1, 1);
-
-# INSERT INTO reply_comment (reply_comment_id, created_date_time, last_modified_date_time, body, comment_id, member_id)
-# VALUES (3, '2023-3-10-11-0', '2023-3-10-11-0', '테스트 대댓글1', 2, 1);
-
 -- 참여상태
 INSERT INTO participation(member_id, recruitment_board_id)
 VALUES (1, 1);
