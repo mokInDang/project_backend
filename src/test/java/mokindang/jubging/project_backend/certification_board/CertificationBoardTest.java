835f8337e93678daac2217a44c4a2323a48ddcf5
최지환 <cjh87467@gmail.com>
2023-10-04 20:00:37 +0900

    test : #411 테스트 코드 추가 메인 이미지가 없는 경우에 대한 예외처리 테스트 추가

--- src/test/java/mokindang/jubging/project_backend/certification_board/CertificationBoardTest.java
+++ src/test/java/mokindang/jubging/project_backend/certification_board/CertificationBoardTest.java
@@ -1,14 +1,16 @@
 package mokindang.jubging.project_backend.certification_board;
 
 import mokindang.jubging.project_backend.certification_board.domain.CertificationBoard;
+import mokindang.jubging.project_backend.member.domain.Member;
 import mokindang.jubging.project_backend.recruitment_board.domain.vo.ContentBody;
 import mokindang.jubging.project_backend.recruitment_board.domain.vo.Title;
-import mokindang.jubging.project_backend.member.domain.Member;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class CertificationBoardTest {
 
@@ -36,5 +38,18 @@ class CertificationBoardTest {
         softly.assertThat(title).isEqualTo(new Title("게시판 제목"));
         softly.assertThat(contentBody).isEqualTo(new ContentBody("게시판 내용"));
         softly.assertAll();
+    }
+
+    @Test
+    @DisplayName("메인 이미지가 존재하지 않는 경우 main image url 조회시 예외를 반환한다.")
+    void validateMainImageUrl() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2023, 11, 12, 0, 0, 0);
+        Member writer = new Member("test1@email.com", "test");
+        CertificationBoard certificationBoard = new CertificationBoard(now, now, writer, "게시판 제목", "게시판 내용");
+
+        //when, then
+        assertThatThrownBy(() -> certificationBoard.getMainImageUrl()).isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미지가 존재하지 않습니다.");
     }
 }
