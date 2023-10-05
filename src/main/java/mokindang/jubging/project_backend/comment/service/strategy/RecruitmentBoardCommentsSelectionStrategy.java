69dd5206f00a8437fc80493e9bfe36e7255c8a17
최지환 <cjh87467@gmail.com>
2023-10-05 18:27:13 +0900

    refactor : #411 댓글 작성 가능 여부 검증 로직 변경

--- src/main/java/mokindang/jubging/project_backend/comment/service/strategy/RecruitmentBoardCommentsSelectionStrategy.java
+++ src/main/java/mokindang/jubging/project_backend/comment/service/strategy/RecruitmentBoardCommentsSelectionStrategy.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import mokindang.jubging.project_backend.comment.domain.Comment;
 import mokindang.jubging.project_backend.comment.repository.CommentRepository;
 import mokindang.jubging.project_backend.comment.service.BoardType;
-import mokindang.jubging.project_backend.comment.service.response.CommentSelectionResponse;
 import mokindang.jubging.project_backend.comment.service.response.MultiCommentSelectionResponse;
+import mokindang.jubging.project_backend.comment.service.response.commentresponse.CommentResponse;
+import mokindang.jubging.project_backend.comment.service.response.commentresponse.RecruitmentBoardCommentResponse;
 import mokindang.jubging.project_backend.member.domain.Member;
 import mokindang.jubging.project_backend.member.service.MemberService;
 import mokindang.jubging.project_backend.recruitment_board.domain.RecruitmentBoard;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 public class RecruitmentBoardCommentsSelectionStrategy implements CommentsSelectionStrategy {
 
     private static final BoardType BOARD_TYPE = BoardType.RECRUITMENT_BOARD;
+
     private final CommentRepository commentRepository;
     private final MemberService memberService;
     private final RecruitmentBoardService recruitmentBoardService;
@@ -27,16 +29,19 @@ public class RecruitmentBoardCommentsSelectionStrategy implements CommentsSelect
     @Override
     public MultiCommentSelectionResponse selectComments(final Long boardId, final Long memberId) {
         List<Comment> commentsByRecruitmentBoard = commentRepository.findCommentByBoardTypeAndBoardId(BOARD_TYPE, boardId);
-        Member member = memberService.findByMemberId(memberId);
         RecruitmentBoard board = recruitmentBoardService.findByIdWithOptimisticLock(boardId);
+        Member member = memberService.findByMemberId(memberId);
         boolean writingCommentPermission = board.isSameRegion(member.getRegion());
-        boolean isWriterParticipatedIn = board.isParticipatedIn(memberId);
-        return new MultiCommentSelectionResponse(convertToRecruitmentBoardCommentSelectionResponse(memberId, commentsByRecruitmentBoard, board, isWriterParticipatedIn), writingCommentPermission);
+        return new MultiCommentSelectionResponse(convertToRecruitmentBoardCommentSelectionResponse(memberId, commentsByRecruitmentBoard, board), writingCommentPermission);
     }
 
-    private List<CommentSelectionResponse> convertToRecruitmentBoardCommentSelectionResponse(final Long memberId, final List<Comment> commentsByRecruitmentBoard, final RecruitmentBoard board, final boolean isWriterParticipatedIn) {
+    private List<CommentResponse> convertToRecruitmentBoardCommentSelectionResponse(final Long memberId, final List<Comment> commentsByRecruitmentBoard, final RecruitmentBoard board) {
         return commentsByRecruitmentBoard.stream()
-                .map(comment -> new CommentSelectionResponse(comment, memberId, board.isSameWriterId(comment.getWriter().getId()), isWriterParticipatedIn))
+                .map(comment -> {
+                    boolean isSameWriterId = board.isSameWriterId(comment.getWriter().getId());
+                    boolean isCommentWriterParticipatedIn = board.isParticipatedIn(comment.getWriter().getId());
+                    return new RecruitmentBoardCommentResponse(comment, memberId, isSameWriterId, isCommentWriterParticipatedIn);
+                })
                 .collect(Collectors.toUnmodifiableList());
     }
 
